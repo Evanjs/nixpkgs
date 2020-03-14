e738e50000050aca7d84dde1335dedd452359acb
Evan Stoll <evanjsx@gmail.com>
2020-03-14 16:45:11 -0400

    polychromatic: use pkgs.meson, not python3Packages.meson

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17940,7 +17940,9 @@ in
 
   poly = callPackage ../data/fonts/poly { };
 
-  polychromatic = python3Packages.callPackage ../applications/misc/polychromatic { };
+  polychromatic = python3Packages.callPackage ../applications/misc/polychromatic {
+    meson = pkgs.meson;
+  };
 
   polytopes_db = callPackage ../data/misc/polytopes_db { };
 
